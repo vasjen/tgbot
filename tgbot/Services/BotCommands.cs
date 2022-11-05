dc1fcd32acb31a1fb5ccb267d151b665758fbc63
Vasjen <vasjenm@gmail.com>
2022-11-05 20:47:56 +0300

    add new branch

---
+++ tgbot/Services/BotCommands.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Types.Payments;
+
+namespace tgbot.services
+{
+    internal static class BotCommands
+
+    {
+        public const string _linkToBuy = "https://www.xbox.com/tr-TR/games/store/p/";
+        public const string _jsStore = "?launchStore=";
+        static int ConvertPriseToUSD(string text)
+        {
+            int result = 0;
+            if (text != null)
+            {
+                text=text.Replace(".", ",");
+                double temp = double.Parse(text);
+                Console.WriteLine($"Price in tyr: {temp}");
+                temp=temp*0.054;
+                Console.WriteLine($"Price in $ USD: {temp}");
+                temp = Math.Round(temp, 2);
+                Console.WriteLine($"Price in $ USD: {temp} after round to 0.01");
+                result = Convert.ToInt32(temp*100);
+                Console.WriteLine($"Final price in $ ISD int32 : {result}");
+                return result;
+            }
+            return result;
+        }
+
+        public static InlineKeyboardMarkup CreatingButtons()
+        {
+            /*
+            InlineKeyboardButton _prev = new InlineKeyboardButton("<<-");
+            InlineKeyboardButton _next = new InlineKeyboardButton("->>");
+            InlineKeyboardButton _action = new InlineKeyboardButton("Buy");
+            _prev.CallbackData="Previous"; _next.CallbackData="Next";
+            _action.CallbackData="invoice";
+            InlineKeyboardButton[] _1stRow = new InlineKeyboardButton[1];
+
+            _1stRow[0]= _action;
+
+
+            InlineKeyboardButton[] _2ndRow = new InlineKeyboardButton[2];
+            _2ndRow[0] = _prev;
+            _2ndRow[1] = _next;
+            */
+            InlineKeyboardMarkup _menu = new(new[] { new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text:"<<--", callbackData:"left"),
+                InlineKeyboardButton.WithCallbackData(text:"-->>", callbackData:"right"),
+            },
+            new[] { InlineKeyboardButton.WithCallbackData(text:"Buy it", callbackData: "invoice")  } });
+            return _menu;
+        }
+        async public static Task NavigationButtons(TeleGramBotClass client)
+            {
+
+
+                await client.botClient.EditMessageMediaAsync(client._idOfSender, client._idOfPhotoMessage, new InputMediaPhoto(media: $"{client.gameCards[client._currentposition].photo}"));
+                await client.botClient.EditMessageCaptionAsync(client._idOfSender, client._idOfPhotoMessage, $"" +
+                   $"{client._currentposition+1} of {client.gameCards.Count}\n " +
+                   $"{client.gameCards[client._currentposition].title} - " +
+                   $"{client.gameCards[client._currentposition].price} ₺", replyMarkup: CreatingButtons());
+            }
+        
+        async public static Task GetCallback (TeleGramBotClass client)
+        {
+            if (client.update.CallbackQuery != null)
+            {
+                Console.WriteLine("Register callback!");
+                await client.botClient.AnswerCallbackQueryAsync(callbackQueryId: client.update.CallbackQuery.Id);
+                Console.WriteLine("Answer send to -> {0}\n" +
+                    "User choise is {1}", client.update.CallbackQuery.Id, client.update.CallbackQuery.Data);
+                if (client.update.CallbackQuery.Data.ToLower()=="invoice")
+                {
+                    int PriceToInvoice = ConvertPriseToUSD(client.gameCards[client._currentposition].price);
+
+                    LabeledPrice game1 = new LabeledPrice($"{client.gameCards[client._currentposition].title}", PriceToInvoice);
+                    LabeledPrice game2 = new LabeledPrice("Service fee: 1$", 100);
+
+                    LabeledPrice[] catalog = new LabeledPrice[2];
+                    catalog[0] = game1;
+                    catalog[1] = game2;
+                    Message sendInvoice = await client.botClient.SendInvoiceAsync(chatId: client.update.CallbackQuery.From.Id,
+                                      title: $"{client.gameCards[client._currentposition].title}",
+                                      description: "Redem code will be send to this email!",
+                                      payload: "somePayload",
+                                      providerToken: "1877036958:TEST:c0c0f684e8b1c6968e6d66a6ed77d2cd46f8be4a",
+                                      //providerToken: "1877036958:TEST:c0c0f684e8b1c6968e6d66a6ed77d2cd46f8be4a",
+                                      currency: "USD",
+                                      prices: catalog,
+                                      needEmail: true,
+                                      startParameter: "exapmle",
+                                      isFlexible: false,
+                                      photoUrl: $"{client.gameCards[client._currentposition].photo}"
+                                      );
+                }
+                if (client.update.CallbackQuery.Data.ToLower()=="previous")
+                {
+
+                    if (client._currentposition >0)
+                    {
+                        client._currentposition--;
+                        await NavigationButtons(client);
+                    }
+                }
+                if (client.update.CallbackQuery.Data.ToLower()=="next")
+                {
+                    if (client._currentposition <client.gameCards.Count-1)
+                    {
+                        client._currentposition++;
+                        await NavigationButtons(client);
+                    }
+                }
+                return;
+            }
+
+        }
+        async public static Task PrechekOut(TeleGramBotClass client)
+        {
+            if (client.update.PreCheckoutQuery is not { } prechek)
+                return;
+            if (prechek !=null)
+            {
+
+                Console.WriteLine("Recieved prechekout requets!");
+                await client.botClient.AnswerPreCheckoutQueryAsync(
+                   preCheckoutQueryId: prechek.Id);
+                Console.WriteLine("Answer sended: to {0}", prechek.Id);
+
+                return;
+            }
+            return;
+        }
+
+        
+    }
+}
---
+++ tgbot/Services/Methods.cs
@@ -0,0 +1,140 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace tgbot.services
+{
+    internal class Methods
+
+    {   
+        internal static IWebDriver GetWebDriver()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.EnableVerboseLogging = false;
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.PageLoadStrategy = PageLoadStrategy.Normal;
+
+            options.AddArgument("--no-sandbox");
+           // options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--disable-crash-reporter");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--disable-in-process-stack-traces");
+            options.AddArgument("--disable-logging");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--log-level=3");
+
+
+            IWebDriver driver = new ChromeDriver(options);
+            
+            return driver;
+        }
+
+        public List<GameCard> FindTheGames(string _searching)
+        {
+            List<GameCard> ResultList = new List<GameCard>();
+            var driver = GetWebDriver();
+            if (_searching.Length != 0)
+            {
+                driver.Navigate().GoToUrl("https://xbox.com/tr-TR/games/all-games");
+                while (driver.FindElements(By.ClassName("m-product-placement-item")).Count == 0) //While dont exist gamecard in catalog script dont run a search and still waiting
+                {
+                    Thread.Sleep(1000);
+                    // Waiting a catalog loading
+                }
+
+                driver.FindElement(By.XPath("//input[contains(@name,'search-field')]")).SendKeys(_searching.ToLower());
+                Thread.Sleep(1000);
+                driver.FindElement(By.XPath("//button[@data-bi-id='n1c1m1r4a3'] ")).Click();
+
+
+                // Count of a games was found 
+                int countOfSearchingResult = driver.FindElements(By.ClassName("c-subheading-4")).Count;
+                if (countOfSearchingResult != 0)
+                {
+
+                    var _resultName = driver.FindElements(By.ClassName("c-subheading-4")); //get <div> block for extracting Title of the game.
+                    var _resultPrice = driver.FindElements(By.ClassName("m-product-placement-item")); //get <div> block for extracting listed price of the game.
+                    var _links = driver.FindElements(By.ClassName("gameDivLink")); //get <div> block for extracting link of the game and label with the price of the game.
+                    var _photoParent = driver.FindElements(By.ClassName("containerIMG"));
+
+
+                    //Saving all the games card in List<>.
+                    for (int i = 0; i < countOfSearchingResult; i++)
+                    {
+                        GameCard card = new GameCard();
+                        card.promo = false;
+                        card.title = _resultName[i].Text;
+
+                        card.photo=_photoParent[i].FindElement(By.ClassName("c-image")).GetAttribute("src");
+                        card.uid = _resultPrice[i].GetAttribute("data-bigid");
+
+
+                        try
+                        {
+                            card.price=_links[i].FindElement(By.ClassName("textpricenew")).Text.TrimStart('₺');
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"{card.title} -> Prob dont have a label with price");
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        ResultList.Add(card);
+
+                    }
+
+                    //Now removing all a cards with incorrect price.
+                    for (int i = 0; i<ResultList.Count; i++)
+                    {
+                        if ((ResultList[i].price==null | ResultList[i].price=="0,00")==true)
+                        {
+                            ResultList.Remove(ResultList[i]);
+                            i--;
+                        }
+
+                    }
+
+                  
+
+                }
+
+            }
+            driver.Quit();
+            return ResultList;
+        }
+       
+        public async Task ShowResultsOfFinding(TeleGramBotClass client)
+        {
+
+            int currentposition = 0; 
+            if (client.gameCards.Count != 0)
+            {
+                
+                Message showResult = await client.botClient.SendPhotoAsync(client.update.Message.From.Id,
+                    photo: client.gameCards[currentposition].photo,
+                    caption: $"{currentposition+1} of {client.gameCards.Count}\n {client.gameCards[currentposition].title}" +
+                    $"for  ${Math.Round(Convert.ToDouble(client.gameCards[currentposition].price)*0.054,2)}",
+                    replyMarkup: BotCommands.CreatingButtons());
+                client._idOfPhotoMessage=showResult.MessageId;
+            }
+            else
+            {
+                Message noRessult = await client.botClient.SendTextMessageAsync(client.update.Message.From.Id, $"Bot can't find the game");
+            }
+
+        }
+         
+    }
+}
